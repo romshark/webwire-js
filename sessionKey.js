dcbd3cb9caaf890d7f1d5358bf22856ac34f3728
Roman Sharkov <roman.sharkov@qbeon.com>
2018-03-05 22:48:37 +0100

    Add session key string representation getter

--- sessionKey.js
+++ sessionKey.js
@@ -1,14 +1,14 @@
 // SessionKey represents a valid session key in binary representation
-export default function SessionKey(str) {
+export default function SessionKey(_str) {
 	let _buf
 
 	// Determine total message size
-	_buf = new ArrayBuffer(str.length)
+	_buf = new ArrayBuffer(_str.length)
 
 	// Write
 	const bytes = new Uint8Array(_buf)
-	for (let i = 0; i < str.length; i++) {
-		let charCode = str.charCodeAt(i)
+	for (let i = 0; i < _str.length; i++) {
+		let charCode = _str.charCodeAt(i)
 		if (charCode < 32 || charCode > 126) throw new Error(
 			`Unsupported session key character (${charCode})`
 		)
@@ -18,6 +18,12 @@ export default function SessionKey(str) {
 	Object.defineProperty(this, "bytes", {
 		get: function() {
 			return bytes
+		}
+	})
+
+	Object.defineProperty(this, "string", {
+		get: function() {
+			return _str
 		}
 	})
 }
