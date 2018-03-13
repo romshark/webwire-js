c2d15ae634604e689f3d3ea3daf869f2bb4072fe
xrei <romanrei.dev@gmail.com>
2018-03-14 01:52:59 +0300

    Fix undefined fetch on node server
    Added node-fetch package to make fetch work on server side

--- examples/chatroom-client-vue/server.js
+++ examples/chatroom-client-vue/server.js
@@ -9,6 +9,9 @@ const microcache = require('route-cache')
 const resolve = file => path.resolve(__dirname, file)
 const { createBundleRenderer } = require('vue-server-renderer')
 
+const fetch = require('node-fetch')
+global.fetch = fetch
+
 const isProd = process.env.NODE_ENV === 'production'
 const useMicroCache = process.env.MICRO_CACHE !== 'false'
 const serverInfo =
